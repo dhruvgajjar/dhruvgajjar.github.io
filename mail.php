a72362f533643df2f4eb12690665cf92f74933d8
dhruvgajjar <30424005+dhruvgajjar@users.noreply.github.com>
2018-08-05 02:35:14 -0500

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+$recipient = "contactme@dhruvgajjar.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
